37d3d47a3f8b15035b61627393042915de063030
Agatha Martins Trento <agathamt78@gmail.com>
2024-11-26 21:58:51 -0300

    Add files via upload

---
+++ recuperação.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+int m = 10,n = 10;
+int i;
+for (i=0; i<=10; i++){
+    scanf("%d", &m, &n[i]);
+}
+for (i=0; i<=10; i++){
+    if(m,n[i<=0]){
+     m,n[i]=1;
+     for(i=0; i<=10; i++){
+     printf("m,n[%d]=%d\n", i, m, n[i]);
+     }
+    }
+  }
+if (m==0 and n==0);
+ printf("m e n sao zero");
+else if (m==1 or n==1);
+ printf("pelo menos eh um");
+else if (m==2 && n==2);
+ printf("m e n sao iguais a dois");
+   return 0;
+}
